44433487e9116b53eeda98d916ee4a56ac586d38
Lucas <lucasfreecs42@gmail.com>
2021-01-18 19:22:07 -0300

    Adicionei uma pasta

---
+++ Variaveis/Exemplo-03.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$nome = "Hcode";
+$site = "www.hcode.com.br";
+
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+////////////////////////////////////////////////////
+$$frutas = array("Abacaxi","Laranja", "Manga");
+
+//Echo $frutas[2];
+
+$nascimento = new DateTime();
+
+ $arquivo = fopen("exemplo-03.php", "r");
+
+ $nulo = NULL;
+
+?>
